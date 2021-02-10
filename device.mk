c777a3313a0c98898a5bf874a8cbce5df5010ab7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 21:58:07 +0100

    fog: Build atrace service
    Change-Id: I89b2e6c308fb4ae63680ef25b56bc20a41a2c2a4
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
