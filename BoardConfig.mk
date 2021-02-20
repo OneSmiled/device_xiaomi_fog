3d9136fbec636143f2166148a1609187b1a02aa1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-21 00:35:32 +0100

    fog: Initial common sepolicies
    * Adapted from xiaomi-sm8250
    
    Co-authored-by: Pig <pig.priv@gmail.com>
    Co-authored-by: Zinadin Zidan <zidan.roking@gmail.com>
    Co-authored-by: Utsav Balar <utsavbalar1231@gmail.com>
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
    Change-Id: Ie64d863d6d36bdebb79d328de9ede5826b1cd486
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,9 @@ USE_SENSOR_MULTI_HAL := true
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
