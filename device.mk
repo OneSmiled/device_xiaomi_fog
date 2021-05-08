57a53c8222825a35896f91c7426a328763de741c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-08 16:32:07 +0200

    fog: Pull in AOSP lights AIDL
    * From LineageOS/android_hardware_interfaces@ee34a8c
    
    Change-Id: Id83b242a850119c83f1b7b2d9b7a74928a7d5e67
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -259,6 +259,13 @@ PRODUCT_PACKAGES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.fog
+
+PRODUCT_PACKAGES += \
+    lights.bengal
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
