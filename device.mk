40d4ef9186209268af0bd19ea4da5526769e1e5e
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:58:57 +0300

    fog: sensors: Avoid target name conflicts
    Change-Id: If7bd530f2be559f238666a842e9ba5f8e050aeb4
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -434,7 +434,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi_fog-multihal \
     libsensorndkbridge
 
 # Servicetracker
