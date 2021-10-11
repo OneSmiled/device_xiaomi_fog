05f56a003e56cb9d5c2c9ab24ef28a626d210038
Arian <arian.kulmer@web.de>
2021-10-11 11:06:41 +0200

    fog: Provide bluetooth and gatekeeper on vendor
    Change-Id: I482503cafaab16d50241ad90b1f51e882727c6f2
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -79,6 +79,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
@@ -233,6 +234,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor
 
 # GPS
 PRODUCT_COPY_FILES += \
