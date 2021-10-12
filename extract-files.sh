ba1c52c9d9840e6f6c8d836ffe1e409d73f09f96
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 18:41:32 +0200

    fog: Patch vendor.qti.hardware.camera.postproc@1.0-service-impl.so
    The modified library NOPs out the instruction to change binder
    threadpool, allowing the library to load and camera to work on S.
    
    [frostg-012: Adapt HEX binder threadpool for our device]
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I1ef0ed29e4a2eb820826e5d7df1fa53c9e3df584
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- extract-files.sh
+++ extract-files.sh
@@ -51,11 +51,14 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            "${SIGSCAN}" -p "13 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
     esac
 }
 
-# Initialize the helper for common device
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
