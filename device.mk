03c045b23b6bc296c67eeb34a4dcd387d8af4dba
intervigilium <intervigil@gmail.com>
2022-03-31 09:13:47 +0530

    fog: Use soong namespace to avoid build conficts
    Change-Id: Ib9037d2d8d082ef786fc461cfd7db0383a281d93
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/xiaomi/fog/fog-vendor.mk)
