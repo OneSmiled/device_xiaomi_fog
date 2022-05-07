b59df5923b2f852c17ca0824ab25568ac7b8fa33
Jabiyeff <cebiyevanar@gmail.com>
2022-05-07 12:36:30 +0400

    fog: Remove duplicate bootctrl namespace
    * Its already defined in Android.bp
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -16,8 +16,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi \
-    hardware/qcom-caf/bootctrl
+    hardware/xiaomi
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
