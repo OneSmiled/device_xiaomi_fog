b1b72028e225d5f45a40910692ba6661d43826f7
Mathieu Chartier <mathieuc@google.com>
2022-06-01 00:41:45 +0100

    fog: Compile system services using speed-profile
    Signed-off-by: shield44 <shield44@pixelexperience.org>
    Change-Id: If720c9cd6429387d332dd3d0e1ddb1efe1da7f81

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
