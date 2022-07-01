28d95c6a9a80f8cfe829eee330aa44811bc02c1e
boedhack99 <boedhack@gmail.com>
2022-07-01 17:56:31 +0700

    fog: vibrator: Move excluded-input-devices to vibrator folder section
    Change-Id: Ie2b058a3dfe75e6a22561f95b3e62f4e3932979f

--- device.mk
+++ device.mk
@@ -472,7 +472,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi_fog
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_PACKAGES += \
