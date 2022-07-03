0c071a048c828698629efc93b3fc1c44e9f714c6
shield44 <shield44@pixelexperience.org>
2022-07-03 15:30:48 +0000

    fog: Import IRSC configs from stock V13.0.10.0
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -245,6 +245,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Keylayout
 PRODUCT_PACKAGES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
