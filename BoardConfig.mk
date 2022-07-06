9334d9c8ef05313c780fd1755d65edeb6beac846
shield44 <shield44@pixelexperience.org>
2022-07-06 11:19:00 +0000

    fog: hidl: Import compatibility matrix and device manifests
    • Also import framework_compatibility_matrix.xml
    
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,12 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_MATRIX_FILE   += $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
