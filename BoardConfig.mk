98e2a7aa07cda6a64a3ab9082f5d986c19bfc967
shield44 <shield44@pixelexperience.org>
2022-07-12 07:24:59 +0000

    fog: Kang Kernel Args and flags from sm6225-common tree
    Signed-off-by: shield44 <shield44@pixelexperience.org>
    Change-Id: Ic41de2406a500ff27498ecdfe3d0371694636821

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,17 +118,31 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_fog
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_fog
 
 # Kernel
-BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
-BOARD_KERNEL_CMDLINE += androidboot.memcg=1
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=4e00000.dwc3
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
-BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0x4a90000
-BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += service_locator.enable=1
-BOARD_KERNEL_CMDLINE += swiotlb=2048
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_IMAGE_NAME  := Image
+BOARD_KERNEL_OFFSET      := 0x00008000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_TAGS_OFFSET        := 0x00000100
+
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
+BOARD_KERNEL_CMDLINE += \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=4e00000.dwc3 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048
+
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_KERNEL_CONFIG := vendor/bengal_defconfig
+TARGET_KERNEL_HEADERS := kernel/xiaomi/bengal
+TARGET_KERNEL_SOURCE := kernel/xiaomi/bengal
 
 # Kernel - Set SELinux to permissive temporarily
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
@@ -137,26 +151,6 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/Image
-
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_RAMDISK_OFFSET := 0x01000000
-
-BOARD_BOOTIMG_HEADER_VERSION := 3
-BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_KERNEL_ARCH := arm64
-
-TARGET_KERNEL_CONFIG := vendor/bengal_defconfig
-TARGET_KERNEL_HEADERS := kernel/xiaomi/bengal
-TARGET_KERNEL_SOURCE := kernel/xiaomi/bengal
-
-# Kernel additional flags
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 
 # Platform
 TARGET_BOARD_PLATFORM := bengal
