26e618eed12e80d4d89027931e9907eda37052fd
xSylla <syllamodder@gmail.com>
2022-07-16 10:46:47 +0400

    fog: Enforce fstab suffix to qcom
    Change-Id: I5b7246196ec57020cdf980a4867b6b9b08ab3087
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,6 +137,7 @@ BOARD_KERNEL_CMDLINE += \
     service_locator.enable=1 \
     swiotlb=2048
 
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_KERNEL_CONFIG := vendor/bengal_defconfig
 TARGET_KERNEL_HEADERS := kernel/xiaomi/fog
