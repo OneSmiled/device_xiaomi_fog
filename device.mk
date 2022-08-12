a1324451737bbd1216d52f95698ead9b5ff437a0
shield44 <shield44@pixelexperience.org>
2022-08-12 09:06:59 +0000

    fog: Build vendor variant of vendor.qti.hardware.bluetooth_audio@2.1
    Signed-off-by: shield44 <shield44@pixelexperience.org>
    Change-Id: I4fbb73d78b04bc8981f189b4e5250d5323356004

--- device.mk
+++ device.mk
@@ -106,6 +106,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     libbluetooth_audio_session \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
