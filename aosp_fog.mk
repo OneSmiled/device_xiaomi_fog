6fd18e1d40de2c0175b3dc73b3aac483618bdffd
shield44 <shield44@pixelexperience.org>
2022-09-17 15:55:26 +0000

    fog: Bringup PixelExperience
    Signed-off-by: shield44 <shield44@pixelexperience.org>

--- aosp_fog.mk
+++ aosp_fog.mk
@@ -12,19 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_fog
+PRODUCT_NAME := aosp_fog
 PRODUCT_DEVICE := fog
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 10C
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="fog-user 12 SKQ1.211103.001 V13.0.2.0.SGEMIXM release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Redmi/fog_global/fog:12/SKQ1.211103.001/V13.0.2.0.SGEMIXM:user/release-keys
